f0c23ce52a807eedd3d6794763b5c486ecdc9799
Kevin Hu <hu.kevin.w@gmail.com>
2020-08-25 09:57:46 -0400

    +template for displaying restaurants

---
+++ client/src/components/PageHeader.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Header } from 'semantic-ui-react'
+
+export default function PageHeader() {
+  return (
+    <Header as='h1'>First Header</Header>
+  )
+}
--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import { Container } from 'semantic-ui-react'
+import PageHeader from '../components/PageHeader'
+import AddNewRestaurant from '../components/AddNewRestaurant'
+import Restauranttable from '../components/RestaurantTable'
 
 export default function Home() {
   return (
     <Container>
+      <PageHeader/>
+      <AddNewRestaurant/>
+      <Restauranttable/>
       <div>home</div>
     </Container>
   )
