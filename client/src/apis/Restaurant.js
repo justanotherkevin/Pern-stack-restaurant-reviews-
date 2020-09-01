08e33c952f51726b0cae162fa7f44af86a838c66
Kevin Hu <hu.kevin.w@gmail.com>
2020-09-01 08:00:41 -0400

    + Restaurant api call

--- client/src/apis/Restaurant.js
+++ client/src/apis/Restaurant.js
@@ -1,5 +1,18 @@
 import axios from "axios";
 
-export default axios.create({
-  baseURL: "http://localhost:3001/api/v1/restaurants",
+const ApiBase = () => axios.create({
+  baseURL: "http://localhost:3030/api/v1/restaurants",
 });
+
+export const fetchAllRestaurants = async () => {
+  try {
+    const res = await ApiBase().get('/')
+    return {
+      status: res.status,
+      restaurants: res.data.data.restaurants,
+      count: res.data.results
+    }
+  } catch(err) {
+    return err
+  }
+}
