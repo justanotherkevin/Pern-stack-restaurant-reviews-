be74bb1b430a33c51318f750908ab97e7aa4057b
Kevin Hu <hu.kevin.w@gmail.com>
2020-09-01 08:00:03 -0400

    + cord package for CORS interaction

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const morgan = require("morgan");
 const { request } = require('express');
@@ -8,6 +9,7 @@ const { request } = require('express');
 const db = require("./db")
 const restaurantController = require('./controllers/restaurantsController');
 
+app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json());
 
