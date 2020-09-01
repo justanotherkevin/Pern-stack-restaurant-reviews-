cbe24338ea3c591f24915633fe20d58ea56508cc
Kevin Hu <hu.kevin.w@gmail.com>
2020-09-01 08:01:20 -0400

    +Restaurant table component

--- client/src/components/RestaurantTable.js
+++ client/src/components/RestaurantTable.js
@@ -1,21 +1,56 @@
-import React from 'react'
+import React, { useEffect, useContext } from 'react'
 import { Header, Table, Rating } from 'semantic-ui-react'
+import { fetchAllRestaurants } from '../apis/Restaurant'
+import { RestaurantContext } from '../context/RestaurantContext'
 
-export default function Restauranttable() {
+export default function Restauranttable(props) {
+  const {restaurants, setRestaurants} = useContext(RestaurantContext)
+  useEffect(()=> {
+    fetchAllRestaurants().then( data => {
+      setRestaurants(data.restaurants)
+    })
+  },[])
+
+  const restaurantkeys = restaurants.length > 0 ?
+    Object.keys(restaurants[0]).filter( word => {
+      return word != "id"
+    }) :
+    []
+
+  const tableHeader = restaurantkeys.map( (header, index) => (
+    <Table.HeaderCell key={`${header}_${index}`}>{header}</Table.HeaderCell>
+  ))
+
+
+  const tableBody = restaurants.map( (resaurant,index) => (
+    <Table.Row key={`res_${index}_row`}>{
+      restaurantkeys.map( (key,index) => {
+        const componentKey = `res_${key}_${index}_cell`
+        if (key === 'price_range') {
+          return (
+            <Table.Cell key={componentKey}>
+              <Header as='h3' textAlign='center'>
+                { "$".repeat(resaurant[key]) }
+              </Header>
+            </Table.Cell>
+          )
+        }
+        return <Table.Cell key={componentKey}>{resaurant[key]}</Table.Cell>
+      })
+    }</Table.Row>
+  ))
+
   return (
     <Table celled padded>
       <Table.Header>
         <Table.Row>
-          <Table.HeaderCell singleLine>Evidence Rating</Table.HeaderCell>
-          <Table.HeaderCell>Effect</Table.HeaderCell>
-          <Table.HeaderCell>Efficacy</Table.HeaderCell>
-          <Table.HeaderCell>Consensus</Table.HeaderCell>
-          <Table.HeaderCell>Comments</Table.HeaderCell>
+          { tableHeader }
         </Table.Row>
       </Table.Header>
 
       <Table.Body>
-        <Table.Row>
+        {tableBody}
+        {/* <Table.Row>
           <Table.Cell>
             <Header as='h2' textAlign='center'>
               A
@@ -23,7 +58,7 @@ export default function Restauranttable() {
           </Table.Cell>
           <Table.Cell singleLine>Power Output</Table.Cell>
           <Table.Cell>
-            <Rating icon='star' defaultRating={3} maxRating={3} />
+            <Rating icon='star' defaultRating={3} maxRating={5} />
           </Table.Cell>
           <Table.Cell textAlign='right'>
             80% <br />
@@ -34,26 +69,7 @@ export default function Restauranttable() {
             muscular creatine levels; there is variability in this increase,
             however, with some nonresponders.
           </Table.Cell>
-        </Table.Row>
-        <Table.Row>
-          <Table.Cell>
-            <Header as='h2' textAlign='center'>
-              A
-            </Header>
-          </Table.Cell>
-          <Table.Cell singleLine>Weight</Table.Cell>
-          <Table.Cell>
-            <Rating icon='star' defaultRating={3} maxRating={3} />
-          </Table.Cell>
-          <Table.Cell textAlign='right'>
-            100% <br />
-            <a href='#'>65 studies</a>
-          </Table.Cell>
-          <Table.Cell>
-            Creatine is the reference compound for power improvement, with numbers
-            from one meta-analysis to assess potency
-          </Table.Cell>
-        </Table.Row>
+        </Table.Row> */}
       </Table.Body>
     </Table>
   )
